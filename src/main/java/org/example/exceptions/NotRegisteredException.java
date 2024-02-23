fb69a70b129b22a460aff538a9cae4db0cd93075
79911 <Petrovlistov@gmail.com>
2024-02-24 00:55:43 +0300

    initial commit

---
+++ src/main/java/org/example/exceptions/NotRegisteredException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException(String playerName) {
+        super("Пользоваетль с именем" + playerName + "не зарегестрирован");
+
+    }
+}
